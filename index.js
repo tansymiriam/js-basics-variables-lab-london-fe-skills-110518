580624b7c69743a80551e73bfd9e8c91e56f7f77
Tansy Roberts <tansymiriam@gmail.com>
2018-12-04 19:30:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
+<<<<<<< HEAD
 let mostProfitableNeighborhood ='Chelsea'
 let companyCeo = 'Susan Smith'
+=======
+let mostProfitableNeighbourhood = 'chelsea'
+>>>>>>> dbeffca883c547e7f1efa4b23aa1c24265cbe1c9
